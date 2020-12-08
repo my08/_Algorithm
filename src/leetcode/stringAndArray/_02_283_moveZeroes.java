6f61dcd8240d5e67b723fececbfad7836ad8a09c
my08 <my08.jung@gmail.com>
2020-12-08 22:49:21 +0900

    283 revised

--- src/leetcode/stringAndArray/_02_283_moveZeroes.java
+++ src/leetcode/stringAndArray/_02_283_moveZeroes.java
@@ -2,6 +2,8 @@ package leetcode.stringAndArray;
 
  class _02_283_moveZeroes {
     public static void moveZeroes(int[] nums) {
+        //Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+        //Memory Usage: 39.3 MB, less than 58.34% of Java online submissions for Move Zeroes.
         int count=0;
         for(int i=0; i<nums.length; i++) {
             if(nums[i]!=0) {
